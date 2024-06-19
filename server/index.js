1a16f190470f7bb7bcc156733d94a58fdef21744
selimnill <mrrafi812345@gmail.com>
2024-06-19 07:59:36 +0600

    update

--- server/index.js
+++ server/index.js
@@ -80,6 +80,8 @@ async function run() {
       );
       res.send(fruitsData);
     });
+    
+    //delete 
     app.delete("/fruits/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
 
